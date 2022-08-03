8eb9cb89c59ce81d12aefb3248dba5d8f8af4437
ingcamilorodriguezdiaz <lcdaniel04@gmail.com>
2022-08-03 17:47:07 -0500

    se agrega al mapper categoria los productos

--- src/main/java/com/tienda/market/domain/Category.java
+++ src/main/java/com/tienda/market/domain/Category.java
@@ -1,10 +1,16 @@
 package com.tienda.market.domain;
 
+import com.tienda.market.persistence.entity.Producto;
+
+import javax.persistence.OneToMany;
+import java.util.List;
+
 public class Category {
     private int categoryId;
     private String category;
     private boolean active;
 
+    private List<Producto> products;
     public int getCategoryId() {
         return categoryId;
     }
@@ -27,5 +33,13 @@ public class Category {
 
     public void setActive(boolean active) {
         this.active = active;
+    }
+
+    public List<Producto> getProducts() {
+        return products;
+    }
+
+    public void setProducts(List<Producto> products) {
+        this.products = products;
     }
 }
