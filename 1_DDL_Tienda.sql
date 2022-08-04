425dc6c020d2df4ed72e82ed16d7d2d2e2e16749
Daniel López Correa <87211525+lcdaniel04@users.noreply.github.com>
2022-08-04 14:30:58 -0500

    Scripts base datos

---
+++ 1_DDL_Tienda.sql
@@ -0,0 +1,82 @@
+-- -----------------------------------------------------
+-- Table "CATEGORIAS"
+-- -----------------------------------------------------
+CREATE TABLE  CATEGORIAS (
+  "id_categoria" SERIAL NOT NULL,
+  "descripcion" VARCHAR(45) NOT NULL,
+  "estado" BOOLEAN NOT NULL,
+  PRIMARY KEY ("id_categoria"));
+
+
+-- -----------------------------------------------------
+-- Table "PRODUCTOS"
+-- -----------------------------------------------------
+CREATE TABLE  PRODUCTOS (
+  "id_producto" SERIAL NOT NULL,
+  "nombre" VARCHAR(45) NULL,
+  "id_categoria" INT NOT NULL,
+  "codigo_barras" VARCHAR(150) NULL,
+  "precio_venta" DECIMAL(16,2) NULL,
+  "cantidad_stock" INT NOT NULL,
+  "estado" BOOLEAN NULL,
+  "cntidad" INT NOT NULL
+  PRIMARY KEY ("id_producto"),
+  CONSTRAINT "fk_PRODUCTOS_CATEGORIAS"
+    FOREIGN KEY ("id_categoria")
+    REFERENCES CATEGORIAS ("id_categoria")
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION);
+
+
+-- -----------------------------------------------------
+-- Table "USUARIOS"
+-- -----------------------------------------------------
+CREATE TABLE  USUARIOS (
+  "id" VARCHAR(20) NOT NULL,
+  "nombre" VARCHAR(40) NOT NULL,
+  "apellidos" VARCHAR(80) NULL,
+  "celular" NUMERIC NULL,
+  "direccion" VARCHAR(80) NULL,
+  "email" VARCHAR(70) NOT NULL,
+  "password" VARCHAR(20) NOT NULL,
+  PRIMARY KEY ("id"));
+
+
+-- -----------------------------------------------------
+-- Table "COMPRAS"
+-- -----------------------------------------------------
+CREATE TABLE  COMPRAS (
+  "id_compra" SERIAL NOT NULL,
+  "id_usuario" VARCHAR(20) NOT NULL,
+  "fecha" TIMESTAMP NULL,
+  "medio_pago" CHAR(1) NULL,
+  "comentario" VARCHAR(300) NULL,
+  "estado" CHAR(1) NULL,
+  PRIMARY KEY ("id_compra"),
+  CONSTRAINT "fk_COMPRAS_USUARIOS1"
+    FOREIGN KEY ("id_usuario")
+    REFERENCES USUARIOS ("id")
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION);
+
+
+-- -----------------------------------------------------
+-- Table "COMPRAS_PRODUCTOS"
+-- -----------------------------------------------------
+CREATE TABLE  COMPRAS_PRODUCTOS (
+  "id_compra" INT NOT NULL,
+  "id_producto" INT NOT NULL,
+  "cantidad" INT NULL,
+  "total" DECIMAL(16,2) NULL,
+  "estado" BOOLEAN NULL,
+  PRIMARY KEY ("id_compra", "id_producto"),
+  CONSTRAINT "fk_COMPRAS_PRODUCTOS_PRODUCTOS1"
+    FOREIGN KEY ("id_producto")
+    REFERENCES PRODUCTOS ("id_producto")
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT "fk_COMPRAS_PRODUCTOS_COMPRAS1"
+    FOREIGN KEY ("id_compra")
+    REFERENCES COMPRAS ("id_compra")
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION);
